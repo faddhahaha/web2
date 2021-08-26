254f67ffbd6f0746b3969c8eaccbe7c07659fc0a
faddhahaha <1391167617@qq.com>
2021-08-26 15:21:28 +0800

    tst

--- src/components/relust.js
+++ src/components/relust.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link} from 'react-router-dom'
 
 const pp
-const pop
 class Relust extends Component{
     constructor(props){
         super(props);
