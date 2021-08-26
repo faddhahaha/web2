58fdeeed31c37a645fe0d62b6f6b631c6f31b16a
faddhahaha <1391167617@qq.com>
2021-08-26 15:08:27 +0800

    oop

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,7 +4,7 @@ import { HashRouter, Route, Link, Redirect, Switch}  from  'react-router-dom';
 const  Battle=lazy(()=>import('./Battle'));
 import Relust from '@/components/relust';
     class App extends Component {
-        constructor(props)
+        constructor(props){
             super(props);
             this.state={navigationValue:location.hash.substring(1)}
         }
@@ -17,8 +17,8 @@ import Relust from '@/components/relust';
             const {navigationValue} =this.state;
             return(
             <div>
-                <HashRouter>
-                        <div style={{ fontSize:'30px',fontWeight:'600',margin:'40px 20px',display:'flex'}} >
+                <Hash Router>
+                        <div style={{ fosfsfntSize:'30px',fontWeight:'600',margin:'40px 20px',display:'flex'}} >
                             
                             <div style={{margin:'0 20px'}} onClick={()=>{ this.onSwich('/ALL') }} >
                             <Link to='/ALL'  style={{color:navigationValue==='/'||navigationValue==='/ALL'||navigationValue==='/ALL/JavaScript'||navigationValue==='/ALL/CSS' ||navigationValue==='/ALL/java'||navigationValue==='/ALL/Ruby' ? 'red':'black'}} >  Popular </Link>   
